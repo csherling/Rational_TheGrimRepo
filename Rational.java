c9c09b255cb44b135ce9c53475b2b8cc6eed001f
Chris <chris@autumn.(none)>
2015-11-17 11:47:33 -0500

    Up to toString()

--- Rational.java
+++ Rational.java
@@ -1,3 +1,22 @@
 public class Rational{
 
+    private double numerator;
+    private double denominator;
+
+    public Rational(){
+	numerator = 0;
+	denominator = 1;
+    }
+
+    public Rational(double newNum, double newDen){
+	numerator = newNum;
+	denominator = newDen;
+    }
+
+    public String toString(){
+	String num = "";
+	num += numerator;
+	num += "/";
+	num += denominator;
+    }
 }
