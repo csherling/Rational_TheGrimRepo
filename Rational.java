5195b6849c7c7d3fdd010bbce4d4431e57fc0f4a
Chris <chris@autumn.(none)>
2015-11-18 11:27:19 -0500

    PHASE 2 COMPLETE

--- Rational.java
+++ Rational.java
@@ -5,9 +5,6 @@
   2015-11-17
 */
 
-
-// Phase 1 Complete
-
 public class  Rational{
 	 
 
@@ -39,7 +36,7 @@ public class  Rational{
 
     //accesors and mutatorsfor each
     public double getNumerator() {
-	return numerator;
+	return numerator; 
     }
 
     public void setNumerator(double numerator) {
@@ -70,24 +67,57 @@ public class  Rational{
     public static double floatValue(Integer s) {
 	return (double)s;
     }
+
+    public void add(Rational r) {
+	this.numerator = this.numerator * r.denominator + r.numerator * this.denominator;
+	this.denominator*=r.denominator;
+    }	
 	
+    public void subtract(Rational r) {
+	this.numerator = this.numerator * r.denominator - r.numerator * this.denominator;
+	this.denominator*=r.denominator;
+    }	
+
+    public static int gcd(double a, double b) {
+	int y = (int)a;
+	int z = (int)b;
+	int c = y;
+	if (y > z)
+	    c = z;
+	while ((c > 0) && 
+	       ((y % c != 0) ||
+		(z % c != 0))
+	       ){
+	    c -= 1;
+	}
+	return c;
+    }
+
+    public void reduce(){
+	double a = this.numerator;
+	double b = this.denominator;
 	
+	this.numerator /= gcd(a, b);
+	this.denominator /= gcd(a, b);
+    }
+
     //Main woohoooooooooooooo #testcalls
     public static void main(String[] args) {
 		
 	Rational r = new Rational(3,4);
 	Rational j = new Rational(5,6);
-	Rational a = new Rational(6,7);
-	Rational b = new Rational(7,8);
+	Rational a = new Rational(3,4);
+	Rational b = new Rational(5,6);
+	Rational c = new Rational(6,8);
 
-	System.out.println("numerator is:" + r.numerator + " denominator is:" + r.denominator);
-	System.out.println("we are dividing this by " + j.numerator + "/" + j.denominator);
-	r.divide(j);
-	System.out.println("Now that we divided, numerator is:" + r.numerator + " denominator is:" + r.denominator);
-	a.multiply(b);
-	System.out.println("Now that we multiplied, numerator is:" + r.numerator + " denominator is:" + r.denominator);
-	int shf = 4; 
-	System.out.println(floatValue(shf));
+	// System.out.println("numerator is:" + r.numerator + " denominator is:" + r.denominator);
+	// System.out.println("we are dividing this by " + j.numerator + "/" + j.denominator);
+	// r.divide(j);
+	// System.out.println("Now that we divided, numerator is:" + r.numerator + " denominator is:" + r.denominator);
+	// a.multiply(b);
+	// System.out.println("Now that we multiplied, numerator is:" + r.numerator + " denominator is:" + r.denominator);
+	// int shf = 4; 
+	// System.out.println(floatValue(shf));
 		
 	/*
 	 * output
@@ -97,7 +127,13 @@ public class  Rational{
 	 Now that we multiplied, numerator is:0.6 denominator is:0.6666666666666666
 	 4.0
 	*/
-		
+	r.add(j);
+	System.out.println("Add: " + r + " Should be 38/24");
+	a.subtract(b);
+	System.out.println("Sub: " + a + " Should be -2/24");
+	System.out.println("GCD OF 12 20: " + gcd(12, 20) + " Should be 4");
+	c.reduce();
+	System.out.println("Reduce: " + c + " Should be 3/4");
     }
 
 }
